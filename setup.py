b3f778595b0b8d66cd8dd71038722e3c3d723ba0
CheerfulUser <ryanridden@gmail.com>
2021-04-02 14:52:08 +1300

    version 1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/CheerfulUser/TESSreduce'
 EMAIL = 'rridden@stsci.edu'
 AUTHOR ='Ryan Ridden-Harper'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = False#'0.1.0'
+VERSION = '1.0.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = ['lightkurve',
